ce29f938ef41508a6b332849bb51a6563b6ad08c
Tyler Miess <tmiess19@gmail.com>
2017-12-12 04:23:10 +0000

    logic now working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,7 +16,39 @@ function apiRoutes(app) {
     // compatibility logic goes here as well
     app.post("/api/friends", function(req, res) {
         console.log(req.body);
-        res.json(req.body);
+
+        // logic:
+
+        // once we have the new survey data stored in an array 
+        // (res.params.scores), we will compare it against every person 
+        // in the friends api
+
+        var friendScores = [];
+        var newFriend = req.body.scores;
+
+        //compare new friend to all of the friends 
+        for (var i = 0; i < friends.length; i++) {
+            var compareFriend = friends[i].scores;
+            var newScore = 0;
+
+            // this is the one-on-one comparison
+            for (var j = 0; i < newFriend.length; j++) {
+                var diff = Math.abs(newFriend[j] - compareFriend[j]);
+                newScore = newScore + diff;
+            }
+
+            friendScores.push(newScore);
+        }
+
+        var bestScore = Math.min(friendScores);
+        var indexBestFriend = friends.indexOf(bestScore);
+        var bestFriend = friends(indexBestFriend);
+
+        // return the new friend's best friend
+        res.json(bestFriend);
+
+        // add new friend to the friends
+        friends.push(req.body);
     });
 }
 
